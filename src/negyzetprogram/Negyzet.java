2aebb05ab16cbf5c9b067b7ba29f8ae3c89f9768
KSZBence <kunszekelybence@gmail.com>
2023-10-09 13:13:32 +0200

    egyemberes verzio

--- src/negyzetprogram/Negyzet.java
+++ src/negyzetprogram/Negyzet.java
@@ -4,4 +4,22 @@ package negyzetprogram;
 public class Negyzet {
     int x;
     int y;
+    int oldal;
+    
+    public void allapot(){
+        System.out.printf("koordináták:%d - %d terulet:%.2f", this.x, this.y, Math.pow(this.oldal, 2));
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setOldal(int oldal) {
+        this.oldal = oldal;
+    }
+    
 }
--- src/negyzetprogram/NegyzetProgram.java
+++ src/negyzetprogram/NegyzetProgram.java
@@ -3,9 +3,16 @@ package negyzetprogram;
 public class NegyzetProgram {
 
     public static void main(String[] args) {
-        
+        new NegyzetProgram().feladat();
     }
     
-    
+    private void feladat(){
+        Negyzet n1 = new Negyzet();
+        n1.setOldal(2);
+        n1.setX(3);
+        n1.setY(4);
+        n1.allapot();
+        
+    }
     
 }
